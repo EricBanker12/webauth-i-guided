f3712dd55062df839d06428ad4fbfd235fbf17b1
ericbanker12 <ericbanker12@gmail.com>
2019-11-11 11:28:24 -0600

    add POST to hash a password

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -8,6 +9,12 @@ router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post('/hash', (req, res) => {
+  // read password, hash it, return to user {password, hash}
+  const password = req.body.password
+  res.json({password, hash: bcrypt.hashSync(password)})
 });
 
 module.exports = router;
